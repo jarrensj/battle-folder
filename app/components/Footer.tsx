d2c088fe21d0f0b428ba95b8d17fbe3fce08225e
Jarren San Jose <sanjose.jarren@gmail.com>
2026-05-11 23:04:23 -0700

    feat: add fan-made disclaimer footer

---
+++ app/components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="text-center p-4">
+      Disclaimer: Pokémon and its trademarks are the property of The Pokémon
+      Company, Nintendo, Game Freak, and Creatures Inc. This is a fan-made
+      site and is not affiliated with them.
+    </footer>
+  );
+}
