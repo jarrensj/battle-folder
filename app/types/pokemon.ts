09b43070dd0701ffff5adcbc42ec1581b84e1c19
Jarren San Jose <sanjose.jarren@gmail.com>
2026-06-02 01:38:01 -0700

    feat: add PokemonSpecies type (#5)

--- app/types/pokemon.ts
+++ app/types/pokemon.ts
@@ -1,3 +1,42 @@
+export type PokemonType =
+  | "Normal"
+  | "Fire"
+  | "Water"
+  | "Electric"
+  | "Grass"
+  | "Ice"
+  | "Fighting"
+  | "Poison"
+  | "Ground"
+  | "Flying"
+  | "Psychic"
+  | "Bug"
+  | "Rock"
+  | "Ghost"
+  | "Dragon"
+  | "Dark"
+  | "Steel"
+  | "Fairy";
+
+export type StatSpread = {
+  hp: number;
+  attack: number;
+  defense: number;
+  spAttack: number;
+  spDefense: number;
+  speed: number;
+};
+
+export type PokemonSpecies = {
+  dex: number;
+  name: string;
+  types: [PokemonType] | [PokemonType, PokemonType];
+  baseStats: StatSpread;
+  weight: number;
+  abilities: string[];
+  spriteUrl: string;
+};
+
 export type RegulationEntry = {
   dex: number;
   name: string;
